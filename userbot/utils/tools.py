a38f8b7be46c7b7e3d58e8c6d38bc40e6e983192
Askarbot <88702146+Askarbot@users.noreply.github.com>
2021-09-17 10:07:28 +0700

    Update tools.py

--- userbot/utils/tools.py
+++ userbot/utils/tools.py
@@ -219,8 +219,8 @@ async def run_cmd(cmd: list) -> tuple[bytes, bytes]:
 
 def post_to_telegraph(title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "Geez-UserBot"
-    auth_url = "https://github.com/vckyou/Geez-UserBot"
+    auth_name = "Skyzuu-Userbot"
+    auth_url = "https://github.com/Askarbot/Skyzuu-Userbot"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=title,

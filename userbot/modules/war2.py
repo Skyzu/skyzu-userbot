21696ad025d89aba93ae59ff26aef2ccf6ebbc52
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-09-29 16:49:21 +0700

    Create war2.py

--- userbot/modules/war2.py
+++ userbot/modules/war2.py
@@ -1,18 +1,21 @@
 from time import sleep
 
+from userbot import CMD_HELP
 from userbot.events import register
 
 
 @register(outgoing=True, pattern=r"^\.sok(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    sleep(2)
+    sleep(1.5)
     await typew.edit("**WOII**")
-    sleep(2)
+    sleep(1.5)
     await typew.edit("**NGENTOT**")
-    sleep(1)
+    sleep(1.5)
     await typew.edit("**KALO MENTAL MASIH PATUNGAN**")
-    sleep(1)
+    sleep(1.5)
     await typew.edit("**GAUSAH SOK KERAS DEH**")
-    sleep(1)
+    sleep(1.5)
     await typew.edit("**GAUSAH SOK KERAS DEH**")
+
+

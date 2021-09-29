777edd0167c87b8efb8a9290bde79efe804b93ea
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-09-29 22:06:45 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -361,7 +361,7 @@ with bot:
 
 
 async def check_alive():
-    await bot.send_file(BOTLOG_CHATID, ALIVE_LOGO, caption=f"**Userbot has been deployed!**\n➖➖➖➖➖➖➖➖➖\n**UserMode:** @{user.username}\n➖➖➖➖➖➖➖➖➖\n**Support:** @Skyzuproject\n➖➖➖➖➖➖➖➖➖")
+    await bot.send_file(BOTLOG_CHATID, ALIVE_LOGO, caption=f"**Userbot has been deployed!**\n➖➖➖➖➖➖➖➖➖\n**Suport:** @Skyzuproject\n➖➖➖➖➖➖➖➖➖")
     return
 
 with bot:

3ea6facffe033cc25b111368ea97a6a783c02a69
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-09-30 10:23:57 +0700

    Create gcast.py

---
+++ userbot/modules/gcast.py
@@ -0,0 +1,86 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+#
+# Ported by Koala @manusiarakitann
+# Recode by @mrismanaziz
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from userbot import CMD_HELP
+from userbot.events import register
+
+
+@register(outgoing=True, pattern=r"^\.gcast(?: |$)(.*)")
+async def gcast(event):
+    xx = event.pattern_match.group(1)
+    if xx:
+        msg = xx
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit("**Berikan Sebuah Pesan atau Reply**")
+        return
+    kk = await event.edit("`Globally Broadcasting Msg...`")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_group:
+            chat = x.id
+            try:
+                done += 1
+                await event.client.send_message(chat, msg)
+            except BaseException:
+                er += 1
+    await kk.edit(
+        f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{er}` **Grup**"
+    )
+
+
+@register(outgoing=True, pattern=r"^\.gucast(?: |$)(.*)")
+async def gucast(event):
+    xx = event.pattern_match.group(1)
+    if xx:
+        msg = xx
+    elif event.is_reply:
+        msg = await event.get_reply_message()
+    else:
+        await event.edit("**Berikan Sebuah Pesan atau Reply**")
+        return
+    kk = await event.edit("`Globally Broadcasting Msg...`")
+    er = 0
+    done = 0
+    async for x in event.client.iter_dialogs():
+        if x.is_user and not x.entity.bot:
+            chat = x.id
+            try:
+                done += 1
+                await event.client.send_message(chat, msg)
+            except BaseException:
+                er += 1
+    await kk.edit(
+        f"**Berhasil Mengirim Pesan Ke** `{done}` **chats, Gagal Mengirim Pesan Ke** `{er}` **chats**"
+    )
+
+
+CMD_HELP.update(
+    {
+        "gcast": "**Plugin : **`gcast`\
+        \n\n  •  **Syntax :** `.gcast` <text/reply media>\
+        \n  •  **Function : **Mengirim Global Broadcast pesan ke Seluruh Grup yang kamu masuk. (Bisa Mengirim Media/Sticker)\
+    "
+    }
+)
+
+
+CMD_HELP.update(
+    {
+        "gucast": "**Plugin : **`gucast`\
+        \n\n  •  **Syntax :** `.gucast` <text/reply media>\
+        \n  •  **Function : **Mengirim Global Broadcast pesan ke Seluruh Private Massage / PC yang masuk. (Bisa Mengirim Media/Sticker)\
+    "
+    }
+)

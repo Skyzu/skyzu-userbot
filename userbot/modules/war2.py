fc8bf3329cf5ad98bc9761dcd7858043644a8a6e
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-09-30 11:06:03 +0700

    Update war2.py

--- userbot/modules/war2.py
+++ userbot/modules/war2.py
@@ -19,11 +19,18 @@ async def typewriter(typew):
     await typew.edit("**GA KEREN LO BEGITU NGENTOT**")
 
 
+@register(outgoing=True, pattern=r"^\.an(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**LO KAN HASIL ZINAH HASIL HUB TERLARANG NAH LO DI BUANG DI TONGSAMPAH NAH MAK LO YG SKRG KASIAN SAMA LO MKNY DI PUNGUT UDH LO CACAT KAKI CACAT TANGAN CACAT MUKA CACAT SEMUAHNY PARAH BET SI KONTOL LO JUGA CACATAN PASTI NANAHAN JUGA,UDH MENDING LO BUNDIR DEH JADI BEBAN ORG DOANG BEGO NGERUGIN MASYARKAT BEGO BOCAH HINA BOCAH HARAM BOCAH AUTIS KEK LO MENDING MATI AJA**")
+
+
+
 CMD_HELP.update(
     {
         "war2": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .sok\
          \n↳ : ngatain orang yang sok keras\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .gucast\
-         \n↳ : Mengirim Pesan Pribadi Secara Global"
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .an\
+         \n↳ : ngatain anak pungut"
     }
 )

73436af10670aed0516229e890846d59074217a5
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-09-30 11:11:54 +0700

    Create war2.py

--- userbot/modules/war2.py
+++ userbot/modules/war2.py
@@ -25,10 +25,19 @@ async def typewriter(typew):
     await typew.edit("**LO KAN HASIL ZINAH HASIL HUB TERLARANG NAH LO DI BUANG DI TONGSAMPAH NAH MAK LO YG SKRG KASIAN SAMA LO MKNY DI PUNGUT UDH LO CACAT KAKI CACAT TANGAN CACAT MUKA CACAT SEMUAHNY PARAH BET SI KONTOL LO JUGA CACATAN PASTI NANAHAN JUGA,UDH MENDING LO BUNDIR DEH JADI BEBAN ORG DOANG BEGO NGERUGIN MASYARKAT BEGO BOCAH HINA BOCAH HARAM BOCAH AUTIS KEK LO MENDING MATI AJA**")
 
 
+@register(outgoing=True, pattern=r"^\.bk(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**BUAT LO KONTOL NIH KALO UDAH HINA GAUSAH SOK SOK NGEHINA HINA GUA KONTOL, GUA TERLALU SUCI BUAT LU YANG HINA ITU ADUHHH. SINI GUA LUDAHIN DLU LU BIAR DIRI LU SUCI KARENA LU TAU LUDAH GUA ITU MULIA SEKALI**")
+
+
+
 CMD_HELP.update(
     {
         "war2": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .sok\
          \n↳ : ngatain orang yang sok keras\
+         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .bk\
+         \n↳ : ngatain bocah hina\
          \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .an\
          \n↳ : ngatain anak pungut"
     }

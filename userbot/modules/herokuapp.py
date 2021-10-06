d9957a003558220481853315ce4e94e60c4c2f6e
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-10-06 10:48:57 +0700

    Update herokuapp.py

--- userbot/modules/herokuapp.py
+++ userbot/modules/herokuapp.py
@@ -134,8 +134,8 @@ async def dyno_usage(dyno):
     """
     Get your account Dyno Usage
     """
-    await dyno.edit("**Prosesing...**")
-    await asyncio.sleep(1.5)
+    await dyno.edit("**Processing...**")
+    await asyncio.sleep(1)
     useragent = (
         "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
         "AppleWebKit/537.36 (KHTML, like Gecko) "

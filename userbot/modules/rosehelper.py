73abb0bc70e0e3083c1b6838fa7c76e55ae1edcd
skyzuu <88702146+Askarbot@users.noreply.github.com>
2021-10-07 08:26:14 +0700

    Au ya

--- userbot/modules/rosehelper.py
+++ userbot/modules/rosehelper.py
@@ -11,9 +11,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 async def usit(e):
     await e.edit(
         f"**Halo {DEFAULTUSER} Jika Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/pikyus1)"
-        "\n[Repo](https://github.com/S/Rose-UserBot)"
-        "\n[Instagram](instagram.com/ndiap04)"
+        "\n[Telegram](t.me/skyzuuuu)"
+        "\n[Repo](https://github.com/S/Skyzuu-UserBot)"
     )
 
 
@@ -21,14 +20,14 @@ async def usit(e):
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/SendiAp/Rose-Userbot/Rose-Userbot/varshelper.txt)"
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/SendiAp/Skyzuu-Userbot/Skyzuu-Userbot/varshelper.txt)"
     )
 
 
 CMD_HELP.update(
     {
         "helper": "`.lhelp`\
-\nUsage: Bantuan Untuk Rose-Userbot.\
+\nUsage: Bantuan Untuk Skyzuu-Userbot.\
 \n`.vars`\
 \nUsage: Melihat Daftar Vars."
     }

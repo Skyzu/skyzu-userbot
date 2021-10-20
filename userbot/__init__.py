4282a481c4d00cb171fed7cf70ba321e66718f0c
Skyzu <92871568+Skyzu@users.noreply.github.com>
2021-10-20 23:16:56 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -99,7 +99,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/Askarbot/skyzu-userbot")
+    "https://github.com/Skyzu/skyzu-userbot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Skyzuu-Userbot")
 

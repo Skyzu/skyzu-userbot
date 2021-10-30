f216bee7b2a3909fda378a3e2be0b6f38d514170
Skyzuu <92871568+Skyzu@users.noreply.github.com>
2021-10-30 15:22:39 +0700

    Update herokuapp.py

--- userbot/modules/herokuapp.py
+++ userbot/modules/herokuapp.py
@@ -183,13 +183,13 @@ async def dyno_usage(dyno):
 
             await dyno.edit(
                 "⚙️ **Dyno Heroku** ⚙️:\n\n"
-                f"➸ `Pemakaian Dyno Hari Ini Untuk` **{ALIVE_NAME}**:\n"
+                f"➸ `Pemakaian Dyno Hari Ini\n"
                 f" ● {AppHours}**h** {AppMinutes}**m** ᴊᴀᴍ/ᴍᴇɴɪᴛ\n"
-                f"➸ `Sisa kuota jam dyno bulan ini`:\n"
+                f"➸ `Sisa dyno bulan ini`:\n"
                 f" ● {hours}**h** {minutes}**m** ᴊᴀᴍ/ᴍᴇɴɪᴛ\n\n"
                 f" **Terpakai Kuota**: [{AppPercentage}%]\n"
                 f" **Total Keseluruhan**: [{percentage}%]\n\n"
-                f"**Owner**:`{ALIVE_NAME}.`\n"
+                f"**Owner**: `{ALIVE_NAME}`\n"
             )
             await asyncio.sleep(20)
             await event.delete()

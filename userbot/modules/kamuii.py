07e841d84096022da7e4dbc0c95b8fd9896bb483
Skyzu <warsitowidodo4@gmail.com>
2021-10-31 23:13:07 +0000

    Skyzuu-Userbot: auto fixes
    Signed-off-by: Skyzu <warsitowidodo4@gmail.com>

--- userbot/modules/kamuii.py
+++ userbot/modules/kamuii.py
@@ -3,7 +3,6 @@ import os
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from userbot import CMD_HELP, TEMP_DOWNLOAD_DIRECTORY, bot
-from userbot import ALIVE_NAME
 from userbot.events import register
 
 

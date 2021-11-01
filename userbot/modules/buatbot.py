f13be825361cb9539f457d56446f01a95d9d5dc3
Skyzuu <92871568+Skyzu@users.noreply.github.com>
2021-11-01 18:31:09 +0700

    Update buatbot.py

--- userbot/modules/buatbot.py
+++ userbot/modules/buatbot.py
@@ -1,6 +1,6 @@
 # nyenyenyenye
-# # FROM skyzu-userbot <https://github.com/Skyzu/skyzu-userbot>
-# @skyzusupport
+# FROM skyzu-userbot <https://github.com/Skyzu/skyzu-userbot>
+# port by koala🐨/@manusiarakitann
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest

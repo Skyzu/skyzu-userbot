88126c063c6b4dde672b32ece82c7e7e50077222
Skyzu <warsitowidodo4@gmail.com>
2021-11-04 10:30:10 +0000

    Skyzuu-Userbot: auto fixes
    Signed-off-by: Skyzu <warsitowidodo4@gmail.com>

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -50,7 +50,7 @@ DEF_UNAPPROVED_MSG = (
     f"**YA KONTOL KARENA SAYA AKAN OTOMATIS MEMBLOKIR KAMU, TUNGGU SAMPAI {DEFAULTUSER} MENERIMA PESAN KAMU** \n"
     "╔═════════════════════╗\n"
     "│○›SUPPORT : [SKYZUSUPPORT](https://t.me/skyzusupport)        \n"
-   f"│○›ᗷY : 𝐒𝐊𝐘𝐙𝐔 𝐔𝐒𝐄𝐑𝐁𝐎𝐓​           \n"
+    f"│○›ᗷY : 𝐒𝐊𝐘𝐙𝐔 𝐔𝐒𝐄𝐑𝐁𝐎𝐓​           \n"
     "╚═════════════════════╝"
 )
 # =================================================================

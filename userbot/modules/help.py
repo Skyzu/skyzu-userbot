69a1db9d90a192ec94fb612a8012fdab024428fb
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2021-11-10 15:18:22 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -35,7 +35,7 @@ async def help(event):
             string += "`" + str(i)
             string += "`\t ❉  "
         await event.edit(
-            "**⚡Sƙყȥυ-υʂҽɾႦσƚ⚡**\n\n"
+            "**⚡𝐒𝐊𝐘𝐙𝐔-𝐔𝐒𝐄𝐑𝐁𝐎𝐓​⚡**\n\n"
             f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
             "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
             f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`"

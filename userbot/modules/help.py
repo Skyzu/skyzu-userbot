f6af4eeba240271701a5d950cdaa0921809c033f
LevinSkuy <88681823+LevinSkuy@users.noreply.github.com>
2021-11-11 05:11:24 +0700

    update

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -35,7 +35,7 @@ async def help(event):
             string += "`" + str(i)
             string += "`\t ❉  "
         await event.edit(
-            "**⚡Sƙყȥυ-υʂҽɾႦσƚ⚡**\n\n"
+            "**⚡lepin-υʂҽɾႦσƚ⚡**\n\n"
             f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
             "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
             f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `.help animasi`"

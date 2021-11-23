f61d26bb8f061a3271ff1b9f5f67e36e43ee209f
Skyzu <warsitowidodo4@gmail.com>
2021-11-23 06:55:14 +0000

    Skyzuu-Userbot: auto fixes
    Signed-off-by: Skyzu <warsitowidodo4@gmail.com>

--- userbot/modules/truth_and_dare.py
+++ userbot/modules/truth_and_dare.py
@@ -3,7 +3,7 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from userbot import bot, CMD_HELP
+from userbot import CMD_HELP, bot
 from userbot.events import register
 
 

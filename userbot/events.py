8a87bb5d280d18c95b9e01757d218e1647627464
LevinSkuy <88681823+LevinSkuy@users.noreply.github.com>
2021-12-21 05:34:43 +0700

    update

--- userbot/events.py
+++ userbot/events.py
@@ -95,11 +95,11 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Sky-Project ERROR**\n"
-                    link = "Silahkan chat: @skyzex"
+                    text = "**Lepin-Project ERROR**\n"
+                    link = "Silahkan chat: @lepingsad"
                     text += "Untuk melaporkan kesalahan"
                     text += f"tinggal teruskan pesan ini {link}.\n"
-                    text += "Skyzu Siap Membantu Kamu\n"
+                    text += "Lepin Siap Membantu Kamu\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"

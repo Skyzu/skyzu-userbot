223b5692dead15522a403352f04019bba5bb518a
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2021-12-22 19:02:15 +0700

    Create cping.py

---
+++ userbot/modules/cping.py
@@ -0,0 +1,58 @@
+
+
+""" Userbot module containing commands related to the \
+    Information Superhighway (yes, Internet). """
+
+import asyncio
+import random
+import time
+import redis
+
+from datetime import datetime
+
+from userbot import (
+    CMD_HELP, 
+    DEVS, 
+    StartTime,
+)
+
+from userbot.events import register
+
+
+cping = [
+    "**Hadir bang** `𓆩79.08𓆪` ",
+    "**Hadir kak** `𓆩99.65𓆪` ",
+    "**Hadir om** `𓆩76.89𓆪` ",
+    "**Hadir sky** `𓆩72.69𓆪` ",
+]
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["Dtk", "Mnt", "Jam", "Hari"]
+
+    while count < 4:
+        count += 50
+        remainder, result = divmod(
+            seconds, 60) if count < 3 else divmod(
+            seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+
+@register(incoming=True, from_users=DEVS, pattern=r"^.cping$")
+async def _(skyzu):
+    await rendy.reply(random.choice(cping))

99458821a9470cdd6513c3a6d1031db8405ed786
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-02-05 22:30:55 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -109,6 +109,7 @@ PMPERMIT_PIC = os.environ.get(
 
 # Bleep Blop, this is a bot ;)
 PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
+PM_LIMIT = int(os.environ.get("PM_LIMIT", 6))
 
 # Send .chatid in any group with all your administration bots (added)
 G_BAN_LOGGER_GROUP = os.environ.get("G_BAN_LOGGER_GROUP", "")

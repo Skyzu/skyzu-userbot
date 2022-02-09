16f39c0df57b2868c22144accc81df69c21543d7
muhammadrizky16 <malikkudus369@gmail.com>
2022-02-09 10:23:49 +0700

    add cmd handler

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from .chrome import chrome, options
 from .format import parse_pre
+from .decorator import skyzu_cmd, skyzu_handler
 from .google_images_download import googleimagesdownload
 from .progress import CancelProcess, progress
 from .tools import (

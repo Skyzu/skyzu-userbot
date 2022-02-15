27e3cfb09b49411e2f0533a8d641137ca50b6ca9
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-02-15 10:42:13 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ async def userbot_on():
         if BOTLOG_CHATID != 0:
             await bot.send_message(
                 BOTLOG_CHATID,
-                f"⚡Skyzu-Userbot Berhasil Diaktfikan⚡\n━━━━━━━━━━━━━━━\n❃ Bot Of : {ALIVE_NAME}\n❃ BotVer : {BOT_VER}@{UPSTREAM_REPO_BRANCH}\n━━━━━━━━━━━━━━━",
+                f"⚡Userbot berhasil di aktifkan\n━━━━━━━━━━━━━━━\n❃ Bot Of : {ALIVE_NAME}\n❃ BotVer : {BOT_VER}@{UPSTREAM_REPO_BRANCH}\n━━━━━━━━━━━━━━━",
             )
     except Exception as e:
         LOGS.info(str(e))

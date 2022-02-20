81f444b6fa8ab8afcd1b2e1a30871e9f746827e3
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-02-20 13:24:53 +0700

    Update welcomes.py

--- userbot/modules/welcomes.py
+++ userbot/modules/welcomes.py
@@ -1,19 +1,17 @@
 from datetime import datetime
 
 from pytz import timezone
+from userbot import CMD_HELP, bot, LOGS, CLEAN_WELCOME, BOTLOG_CHATID
 from telethon.events import ChatAction
-
-from userbot import BOTLOG_CHATID, CLEAN_WELCOME, CMD_HELP, LOGS, bot
-from userbot.events import skyzu_cmd
+from userbot.events import rose_cmd
+from userbot import CMD_HANDLER as cmd
 
 
 @bot.on(ChatAction)
 async def welcome_to_chat(event):
     try:
-        from userbot.modules.sql_helper.welcome_sql import (
-            get_current_welcome_settings,
-            update_previous_welcome,
-        )
+        from userbot.modules.sql_helper.welcome_sql import get_current_welcome_settings
+        from userbot.modules.sql_helper.welcome_sql import update_previous_welcome
     except AttributeError:
         return
     cws = get_current_welcome_settings(event.chat_id)
@@ -22,12 +20,12 @@ async def welcome_to_chat(event):
         user_joined=True,
         user_left=False,
         user_kicked=False"""
-        if (event.user_joined or event.user_added) and not (await event.get_user()).bot:
+        if (event.user_joined
+                or event.user_added) and not (await event.get_user()).bot:
             if CLEAN_WELCOME:
                 try:
-                    await event.client.delete_messages(
-                        event.chat_id, cws.previous_welcome
-                    )
+                    await event.client.delete_messages(event.chat_id,
+                                                       cws.previous_welcome)
                 except Exception as e:
                     LOGS.warn(str(e))
             a_user = await event.get_user()
@@ -59,7 +57,8 @@ async def welcome_to_chat(event):
             title = chat.title if chat.title else "Grup Ini"
             participants = await event.client.get_participants(chat)
             count = len(participants)
-            mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
+            mention = "[{}](tg://user?id={})".format(a_user.first_name,
+                                                     a_user.id)
             my_mention = "[{}](tg://user?id={})".format(me.first_name, me.id)
             first = a_user.first_name
             last = a_user.last_name
@@ -79,35 +78,31 @@ async def welcome_to_chat(event):
             file_media = None
             current_saved_welcome_message = None
             if cws and cws.f_mesg_id:
-                msg_o = await event.client.get_messages(
-                    entity=BOTLOG_CHATID, ids=int(cws.f_mesg_id)
-                )
+                msg_o = await event.client.get_messages(entity=BOTLOG_CHATID,
+                                                        ids=int(cws.f_mesg_id))
                 file_media = msg_o.media
                 current_saved_welcome_message = msg_o.message
             elif cws and cws.reply:
                 current_saved_welcome_message = cws.reply
             current_message = await event.reply(
-                current_saved_welcome_message.format(
-                    mention=mention,
-                    title=title,
-                    count=count,
-                    first=first,
-                    last=last,
-                    fullname=fullname,
-                    username=username,
-                    userid=userid,
-                    my_first=my_first,
-                    my_last=my_last,
-                    my_fullname=my_fullname,
-                    my_username=my_username,
-                    my_mention=my_mention,
-                ),
-                file=file_media,
-            )
+                current_saved_welcome_message.format(mention=mention,
+                                                     title=title,
+                                                     count=count,
+                                                     first=first,
+                                                     last=last,
+                                                     fullname=fullname,
+                                                     username=username,
+                                                     userid=userid,
+                                                     my_first=my_first,
+                                                     my_last=my_last,
+                                                     my_fullname=my_fullname,
+                                                     my_username=my_username,
+                                                     my_mention=my_mention),
+                file=file_media)
             update_previous_welcome(event.chat_id, current_message.id)
 
 
-@skyzu_cmd(pattern="setwelcome(?: |$)(.*)")
+@skyzu_cmd(pattern="setwelcome(?: |$)(.*)"))
 async def save_welcome(event):
     try:
         from userbot.modules.sql_helper.welcome_sql import add_welcome_setting
@@ -119,72 +114,70 @@ async def save_welcome(event):
     if msg and msg.media and not string:
         if BOTLOG_CHATID:
             await event.client.send_message(
-                BOTLOG_CHATID,
-                f"#WELCOME \nID GRUP: {event.chat_id}"
-                "\nMemasang Pesan Perintah Welcome Digrup, Ini Adalah Catatan Pesan Welcome "
-                "Mohon Jangan Dihapus!",
+                BOTLOG_CHATID, f"#ROSE #WELCOME\n»**ID-GRUP:** {event.chat_id}"
+                "\n» __Memasang Pesan Welcome Digroups__ , __Mohon Jangan Dihapus__"
             )
             msg_o = await event.client.forward_messages(
-                entity=BOTLOG_CHATID, messages=msg, from_peer=event.chat_id, silent=True
-            )
+                entity=BOTLOG_CHATID,
+                messages=msg,
+                from_peer=event.chat_id,
+                silent=True)
             msg_id = msg_o.id
         else:
             return await event.edit(
-                "`Untuk membuat media sebagai pesan Welcome, BOTLOG_CHATID Harus disetel.`"
+                "🚧 `Untuk membuat media sebagai pesan Welcome, BOTLOG_CHATID Harus disetel...`"
             )
     elif event.reply_to_msg_id and not string:
         rep_msg = await event.get_reply_message()
         string = rep_msg.text
-    success = "`Berhasil Menyimpan Pesan Welcome {}`"
+    success = "✔️ `Berhasil Menyimpan Pesan Welcome {}`..."
     if add_welcome_setting(event.chat_id, 0, string, msg_id) is True:
-        await event.edit(success.format("Disini"))
+        await event.edit(success.format('Disini'))
     else:
-        await event.edit(success.format("Disini"))
+        await event.edit(success.format('Disini'))
 
 
-@skyzu_cmd(pattern="checkwelcome$")
+@skyzu_cmd(pattern="checkwelcome(?: |$)(.*)"))
 async def show_welcome(event):
     try:
         from userbot.modules.sql_helper.welcome_sql import get_current_welcome_settings
     except AttributeError:
-        return await event.edit("`Running on Non-SQL mode!`")
+        return await event.edit("`Berjalan pada mode Non-SQL...`")
     cws = get_current_welcome_settings(event.chat_id)
     if not cws:
-        return await event.edit("`Disini Tidak Ada Pesan Welcome Yang Anda Simpan `")
+        return await event.edit("✖️ `Disini Tidak Ada Pesan Welcome Yang Anda Simpan...`")
     elif cws and cws.f_mesg_id:
-        msg_o = await event.client.get_messages(
-            entity=BOTLOG_CHATID, ids=int(cws.f_mesg_id)
-        )
-        await event.edit("`Anda Telah Membuat Pesan Welcome Disini`")
+        msg_o = await event.client.get_messages(entity=BOTLOG_CHATID,
+                                                ids=int(cws.f_mesg_id))
+        await event.edit(
+            "📝 `Anda Telah Membuat Pesan Welcome Disini...`")
         await event.reply(msg_o.message, file=msg_o.media)
     elif cws and cws.reply:
-        await event.edit("`Anda Telah Membuat Pesan Welcome Disini`")
+        await event.edit(
+            "📝 `Anda Telah Membuat Pesan Welcome Disini...`")
         await event.reply(cws.reply)
 
 
-@skyzu_cmd(pattern="rmwelcome$")
+@skyzu_cmd(pattern=rmwelcome(?: |$)(.*)"))
 async def del_welcome(event):
     try:
         from userbot.modules.sql_helper.welcome_sql import rm_welcome_setting
     except AttributeError:
-        return await event.edit("`Running on Non-SQL mode!`")
+        return await event.edit("`Berjalan pada mode Non-SQL...`")
     if rm_welcome_setting(event.chat_id) is True:
-        await event.edit("`Menghapus Pesan Welcome Berhasil Dilakukan`")
+        await event.edit("✔️ `Menghapus Pesan Welcome Berhasil Dilakukan...`")
     else:
-        await event.edit("`Anda Tidak Menyimpan Pesan Welcome Apapun Disini`")
+        await event.edit("📛 `Anda Tidak Menyimpan Pesan Welcome Apapun Disini...`")
 
 
-CMD_HELP.update(
-    {
-        "welcome": ">`{cmd}setwelcome` <pesan welcome> atau balas ke pesan ketik `.setwelcome`"
-        "\nUsage: Menyimpan pesan welcome digrup."
-        "\n\nFormat Variabel yang bisa digunakan dipesan welcome:"
-        "\n`{mention}, {title}, {count}, {first}, {last}, {fullname}, "
-        "{userid}, {username}, {my_first}, {my_fullname}, {my_last}, "
-        "{my_mention}, {my_username}`"
-        "\n\n>`{cmd}checkwelcome`"
-        "\nUsage: Check pesan welcome yang anda simpan."
-        "\n\n>`{cmd}rmwelcome`"
-        "\nUsage: Menghapus pesan welcome yang anda simpan."
-    }
-)
+CMD_HELP.update({
+    "welcome":
+    f"**✘ Format Variabel Pesan Welcome :\
+\n\n  •  **Perintah :** `{cmd}setwelcome`\
+  \n  •  **Fungsi :** Membuat Pesan Welcome Digrpups.\
+\n\n  •  **Perintah :** `{cmd}checkwelcome`\
+  \n  •  **Fungsi : ** Melihat Pesan welcome yang dipasang Digroups.\
+\n\n  •  **Perintah :** `{cmd}rmwelcome`\
+  \n  •  **Fungsi : ** Menghapus Pesan Welcome Yang Disimpan.\
+\n\n  •  **Perintah :** `/get format` @skyzusupport\
+  \n  •  **Fungsi :** Mendapatkan Format Variable Digrub @skyzusupport"})

c5a97f5cbb45e5b8e48b2e42359374a3281e8dd4
Skyzu <warsitowidodo4@gmail.com>
2022-02-21 17:10:45 +0000

    Skyzuu-Userbot: auto fixes
    Signed-off-by: Skyzu <warsitowidodo4@gmail.com>

--- userbot/modules/tiktok.py
+++ userbot/modules/tiktok.py
@@ -1,6 +1,6 @@
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
-from userbot import CMD_HELP, bot, CMD_HANDLER as cmd
+from userbot import CMD_HELP, bot
 from userbot.utils import skyzu_cmd
 
 

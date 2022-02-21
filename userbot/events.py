2901513d4cb7647d1e9ef26836cba1b125d18ba9
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-02-21 21:50:31 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -14,7 +14,7 @@ from traceback import format_exc
 
 from telethon import events
 
-from userbot.events import skyzu_cmd
+from userbot.utils import skyzu_cmd
 from userbot import LOGSPAMMER, bot
 
 

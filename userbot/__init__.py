a45d1ce56f91dc33badbbf593aab35887b05dc63
Skyzu <warsitowidodo4@gmail.com>
2022-02-24 02:15:46 +0000

    Skyzuu-Userbot: auto fixes
    Signed-off-by: Skyzu <warsitowidodo4@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -474,7 +474,9 @@ with bot:
             try:
                 tgbotusername = BOT_USERNAME
                 if tgbotusername is not None:
-                    results = await event.client.inline_query(tgbotusername, "{BOT_USERNAME}")
+                    results = await event.client.inline_query(
+                        tgbotusername, "{BOT_USERNAME}"
+                    )
                     await results[0].click(
                         event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
                     )

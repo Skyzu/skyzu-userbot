2578b6b4031358020af6b09eaeba5880c59c2355
Skyzu <warsitowidodo4@gmail.com>
2022-02-24 00:52:38 +0000

    Skyzuu-Userbot: auto fixes
    Signed-off-by: Skyzu <warsitowidodo4@gmail.com>

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,6 +7,7 @@
 
 import sys
 from importlib import import_module
+
 from telethon.tl.functions.channels import InviteToChannelRequest as Addbot
 
 from userbot import ALIVE_NAME, BOT_VER, BOTLOG_CHATID, LOGS, UPSTREAM_REPO_BRANCH, bot

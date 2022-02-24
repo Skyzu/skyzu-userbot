83d0066f355fa97292968f8c708c693e8cc980d5
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-02-24 07:51:57 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -7,6 +7,8 @@
 
 import sys
 from importlib import import_module
+from telethon.tl.functions.channels import InviteToChannelRequest as Addbot
+
 
 from userbot import ALIVE_NAME, BOT_VER, BOTLOG_CHATID, LOGS, UPSTREAM_REPO_BRANCH, bot
 from userbot.modules import ALL_MODULES

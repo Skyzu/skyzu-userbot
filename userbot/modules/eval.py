ce96f211e49df54d53e317ddc5a61a5d38b02037
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-02-25 08:06:32 +0700

    Update eval.py

--- userbot/modules/eval.py
+++ userbot/modules/eval.py
@@ -143,11 +143,42 @@ async def terminal_runner(term):
         await term.edit(f"`{curruser}:~$ {command}\n{result}`")
 
 
+
+@skyzu_cmd(pattern="json$")
+async def _(event):
+    if event.fwd_from:
+        return
+    the_real_message = None
+    reply_to_id = None
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        the_real_message = previous_message.stringify()
+        reply_to_id = event.reply_to_msg_id
+    else:
+        the_real_message = event.stringify()
+        reply_to_id = event.message.id
+    if len(the_real_message) > 4096:
+        with io.BytesIO(str.encode(the_real_message)) as out_file:
+            out_file.name = "json.text"
+            await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                thumb="userbot/resources/logo.jpg",
+                allow_cache=False,
+                reply_to=reply_to_id,
+            )
+            await event.delete()
+    else:
+        await event.edit("`{}`".format(the_real_message))
+
+
 CMD_HELP.update(
     {
         "eval": f">`{cmd}eval print('world')`" "\nUsage: Just like exec.",
         "exec": f">`{cmd}exec print('hello')`" "\nUsage: Execute small python scripts.",
         "term": f">`{cmd}term <cmd>`"
+        "json": f">`{cmd}json <cmd>
         "\nUsage: Run bash commands and scripts on your server.",
     }
 )

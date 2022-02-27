e426bdda7a115d73fd7872007831c1ad8266a438
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-02-28 06:36:27 +0700

    Update sangmata.py

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -1,66 +1,85 @@
-from asyncio.exceptions import TimeoutError
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+import asyncio
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest
 
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP, bot
-from userbot.utils import skyzu_cmd
+from userbot import CMD_HELP
+from userbot.utils import (
+    _format,
+    edit_delete,
+    edit_or_reply,
+    get_user_from_event,
+    skyzu_cmd,
+)
 
 
-@skyzu_cmd(pattern="sg(?: |$)(.*)")
-async def lastname(steal):
-    if steal.fwd_from:
+@skyzu_cmd(pattern="sg(u)?(?:\s|$)([\s\S]*)")
+async def _(event):
+    input_str = "".join(event.text.split(maxsplit=1)[1:])
+    reply_message = await event.get_reply_message()
+    if not input_str and not reply_message:
+        await edit_delete(event, "**Mohon Reply Ke Pesan Pengguna.**", 90)
+    user, rank = await get_user_from_event(event, secondgroup=True)
+    if not user:
         return
-    if not steal.reply_to_msg_id:
-        await steal.edit("```Mohon Balas Ke Pesan Pengguna.```")
-        return
-    message = await steal.get_reply_message()
+    uid = user.id
     chat = "@SangMataInfo_bot"
-    user_id = message.sender.id
-    id = f"/search_id {user_id}"
-    if message.sender.bot:
-        await steal.edit("```Balas Ke Pesan Pengguna Yang Sebenarnya.```")
-        return
-    await steal.edit("```Lu siapa si ngentot, gua intip sabi laa..```")
-    try:
-        async with bot.conversation(chat) as conv:
+    manevent = await edit_or_reply(event, "`Processing...`")
+    async with event.client.conversation(chat) as conv:
+        try:
+            await conv.send_message(f"/search_id {uid}")
+        except YouBlockedUserError:
+            await event.client(UnblockRequest(chat))
+            await conv.send_message(f"/search_id {uid}")
+        responses = []
+        while True:
             try:
-                msg = await conv.send_message(id)
-                r = await conv.get_response()
-                response = await conv.get_response()
-            except YouBlockedUserError:
-                await steal.reply("```Mohon Unblock @sangmatainfo_bot Dan Coba Lagi```")
-                return
-            if r.text.startswith("Name"):
-                respond = await conv.get_response()
-                await steal.edit(f"`{r.message}`")
-                await steal.client.delete_messages(
-                    conv.chat_id, [msg.id, r.id, response.id, respond.id]
-                )
-                return
-            if response.text.startswith("No records") or r.text.startswith(
-                "No records"
-            ):
-                await steal.edit(
-                    "```Saya Tidak Menemukan Informasi Pengguna Ini, Pengguna Ini Belum Pernah Mengganti Nama Sebelumnya```"
-                )
-                await steal.client.delete_messages(
-                    conv.chat_id, [msg.id, r.id, response.id]
-                )
-                return
-            else:
-                respond = await conv.get_response()
-                await steal.edit(f"```{response.message}```")
-            await steal.client.delete_messages(
-                conv.chat_id, [msg.id, r.id, response.id, respond.id]
-            )
-    except TimeoutError:
-        return await steal.edit("`Saya Sedang Sakit Mohon Maaf`")
+                response = await conv.get_response(timeout=2)
+            except asyncio.TimeoutError:
+                break
+            responses.append(response.text)
+        await event.client.send_read_acknowledge(conv.chat_id)
+    if not responses:
+        await edit_delete(manevent, "**Orang Ini Belum Pernah Mengganti Namanya**", 90)
+    if "No records found" in responses:
+        await edit_delete(manevent, "**Orang Ini Belum Pernah Mengganti Namanya**", 90)
+    names, usernames = await sangamata_seperator(responses)
+    cmd = event.pattern_match.group(1)
+    risman = None
+    check = usernames if cmd == "u" else names
+    for i in check:
+        if risman:
+            await event.reply(i, parse_mode=_format.parse_pre)
+        else:
+            risman = True
+            await manevent.edit(i, parse_mode=_format.parse_pre)
+
+
+async def sangamata_seperator(sanga_list):
+    for i in sanga_list:
+        if i.startswith("🔗"):
+            sanga_list.remove(i)
+    s = 0
+    for i in sanga_list:
+        if i.startswith("Username History"):
+            break
+        s += 1
+    usernames = sanga_list[s:]
+    names = sanga_list[:s]
+    return names, usernames
 
 
 CMD_HELP.update(
     {
-        "sangmata": f"`{cmd}sg`\
-          \nUsage: Mendapatkan Riwayat Nama Pengguna."
+        "sangmata": f"**Plugin : **`sangmata`\
+        \n\n  •  **Syntax :** `{cmd}sg` <sambil reply chat>\
+        \n  •  **Function : **Mendapatkan Riwayat Nama Pengguna selama di telegram.\
+        \n\n  •  **Syntax :** `{cmd}sgu` <sambil reply chat>\
+        \n  •  **Function : **Mendapatkan Riwayat Username Pengguna selama di telegram.\
+    "
     }
 )

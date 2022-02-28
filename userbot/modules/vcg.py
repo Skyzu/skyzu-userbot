9b2de41c185aea62c9315356eebecb82f6faaac0
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-02-28 10:14:39 +0700

    Update vcg.py

--- userbot/modules/vcg.py
+++ userbot/modules/vcg.py
@@ -1,30 +1,23 @@
-# Thanks Full To Team Ultroid
+# Thanks @mrismanaziz <https://github.com/mrismanaziz/Man-userbot>
 # Ported By @skyzu
 
 from telethon.tl.functions.channels import GetFullChannelRequest as getchat
 from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
 from telethon.tl.functions.phone import DiscardGroupCallRequest as stopvc
+from telethon.tl.functions.phone import EditGroupCallTitleRequest as settitle
 from telethon.tl.functions.phone import GetGroupCallRequest as getvc
 from telethon.tl.functions.phone import InviteToGroupCallRequest as invitetovc
 
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
-from userbot.utils import skyzu_cmd
-
-NO_ADMIN = "`Maaf Kamu Bukan Admin!"
-
-
-def vcmention(user):
-    full_name = get_display_name(user)
-    if not isinstance(user, types.User):
-        return full_name
-    return f"[{full_name}](tg://user?id={user.id})"
+from userbot.events import register
+from userbot.utils import edit_delete, edit_or_reply, skyzy_cmd
 
 
-async def get_call(sky):
-    sky = await sky.client(getchat(sky.chat_id))
-    await sky.client(getvc(sky.full_chat.call))
-    return hehe.call
+async def get_call(event):
+    mm = await event.client(getchat(event.chat_id))
+    xx = await event.client(getvc(mm.full_chat.call, limit=1))
+    return xx.call
 
 
 def user_list(l, n):
@@ -33,60 +26,92 @@ def user_list(l, n):
 
 
 @skyzu_cmd(pattern="startvc$")
-async def start_voice(td):
-    chat = await td.get_chat()
+@register(pattern=r"^\.startvcs$", sudo=True)
+async def start_voice(c):
+    me = await c.client.get_me()
+    chat = await c.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        return await td.edit(NO_ADMIN)
+        await edit_delete(c, f"**Maaf {me.first_name} Bukan Admin 👮**")
+        return
     try:
-        await td.client(startvc(td.chat_id))
-        await td.edit("`Voice Chat Started...`")
+        await c.client(startvc(c.chat_id))
+        await edit_or_reply(c, "`Voice Chat Started...`")
     except Exception as ex:
-        await td.edit(f"`{str(ex)}`")
+        await edit_delete(c, f"**ERROR:** `{ex}`")
 
 
 @skyzu_cmd(pattern="stopvc$")
-async def stop_voice(td):
-    chat = await td.get_chat()
+@register(pattern=r"^\.stopvcs$", sudo=True)
+async def stop_voice(c):
+    me = await c.client.get_me()
+    chat = await c.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
 
     if not admin and not creator:
-        return await td.edit(NO_ADMIN)
+        await edit_delete(c, f"**Maaf {me.first_name} Bukan Admin 👮**")
+        return
     try:
-        await td.client(stopvc(await get_call(td)))
-        await td.edit("`Voice Chat Stopped...`")
+        await c.client(stopvc(await get_call(c)))
+        await edit_or_reply(c, "`Voice Chat Stopped...`")
     except Exception as ex:
-        await td.edit(f"`{str(ex)}`")
+        await edit_delete(c, f"**ERROR:** `{ex}`")
 
 
 @skyzu_cmd(pattern="vcinvite")
-async def vc_invite(td):
-    await td.edit("`Sedang Mengivinte Member...`")
+async def _(c):
+    xxnx = await edit_or_reply(c, "`Inviting Members to Voice Chat...`")
     users = []
     z = 0
-    async for x in td.client.iter_participants(td.chat_id):
+    async for x in c.client.iter_participants(c.chat_id):
         if not x.bot:
             users.append(x.id)
-    hmm = list(user_list(users, 6))
-    for p in hmm:
+    botman = list(user_list(users, 6))
+    for p in botman:
         try:
-            await td.client(invitetovc(call=await get_call(td), users=p))
+            await c.client(invitetovc(call=await get_call(c), users=p))
             z += 6
         except BaseException:
             pass
-    await td.edit(f"`Invited {z} users`")
+    await xxnx.edit(f"`{z}` **Orang Berhasil diundang ke VCG**")
+
+
+@skyzu_cmd(pattern="vctitle(?: |$)(.*)")
+@register(pattern=r"^\.cvctitle$", sudo=True)
+async def change_title(e):
+    title = e.pattern_match.group(1)
+    me = await e.client.get_me()
+    chat = await e.get_chat()
+    admin = chat.admin_rights
+    creator = chat.creator
+
+    if not title:
+        return await edit_delete(e, "**Silahkan Masukan Title Obrolan Suara Grup**")
+
+    if not admin and not creator:
+        await edit_delete(e, f"**Maaf {me.first_name} Bukan Admin 👮**")
+        return
+    try:
+        await e.client(settitle(call=await get_call(e), title=title.strip()))
+        await edit_or_reply(e, f"**Berhasil Mengubah Judul VCG Menjadi** `{title}`")
+    except Exception as ex:
+        await edit_delete(e, f"**ERROR:** `{ex}`")
 
 
 CMD_HELP.update(
     {
-        "vcg": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}startvc`\
-         \n↳ : Start Group Call in a group.\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}stopvc`\
-         \n↳ : `Stop Group Call in a group.`\
-         \n𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}vcinvite`\
-         \n↳ : Invite all members of group in Group Call. (You must be joined)."
+        "vcg": f"**Plugin : **`vcg`\
+        \n\n  •  **Syntax :** `{cmd}startvc`\
+        \n  •  **Function : **Untuk Memulai voice chat group\
+        \n\n  •  **Syntax :** `{cmd}stopvc`\
+        \n  •  **Function : **Untuk Memberhentikan voice chat group\
+        \n\n  •  **Syntax :** `{cmd}vctitle` <title vcg>\
+        \n  •  **Function : **Untuk Mengubah title/judul voice chat group\
+        \n\n  •  **Syntax :** `{cmd}vcinvite`\
+        \n  •  **Function : **Mengundang Member group ke voice chat group\
+    "
     }
 )

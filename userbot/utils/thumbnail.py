33533f4031251473b4ed3a859664622d5973e94c
Skyzu <warsitowidodo4@gmail.com>
2022-03-06 11:31:10 +0000

    Skyzuu-Userbot: auto fixes
    Signed-off-by: Skyzu <warsitowidodo4@gmail.com>

--- userbot/utils/thumbnail.py
+++ userbot/utils/thumbnail.py
@@ -21,9 +21,7 @@ async def gen_thumb(thumbnail, title, userid, ctitle):
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
-                f = await aiofiles.open(
-                    f"resources/thumb{userid}.png", mode="wb"
-                )
+                f = await aiofiles.open(f"resources/thumb{userid}.png", mode="wb")
                 await f.write(await resp.read())
                 await f.close()
     theme = random.choice(themes)

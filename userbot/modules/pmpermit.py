5d962fe3fd6d2845bac5e8d06affe7292f5f5907
SkyzuProject <92871568+Skyzu@users.noreply.github.com>
2022-03-12 08:30:53 +0700

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -49,7 +49,7 @@ DEF_UNAPPROVED_MSG = (
     "**TOLONG JANGAN MELAKUKAN SPAM CHAT KEPADA MAJIKAN SAYA** \n"
     f"**YA KONTOL KARENA SAYA AKAN OTOMATIS MEMBLOKIR KAMU, TUNGGU SAMPAI {DEFAULTUSER} MENERIMA PESAN KAMU** \n"
     "╔═════════════════════╗\n"
-    "│○›Support : @skyzusupport      \n"
+    "│○›Support : @ProjectSkyzu      \n"
     f"│○›ᗷy : 𝐒𝐊𝐘𝐙𝐔 𝐔𝐒𝐄𝐑𝐁𝐎𝐓​           \n"
     "╚═════════════════════╝"
 )

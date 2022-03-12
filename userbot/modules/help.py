ae23616cef0954ec880a9887e6409e7d30461d3a
ᴷʸʸ <89422473+muhammadrizky16@users.noreply.github.com>
2022-03-12 18:00:39 +0700

    gitu dulu

---
+++ userbot/modules/help.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+""" Userbot help command """
+
+import asyncio
+from platform import uname
+
+from userbot import ALIVE_NAME
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
+from userbot.utils import skyzu_cmd
+
+modules = CMD_HELP
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
+
+
+@skyzu_cmd(pattern="help(?: |$)(.*)")
+async def help(event):
+    """For .help command,"""
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit("`Command` **Tidak Valid**")
+            await asyncio.sleep(200)
+            await event.delete()
+    else:
+        string = ""
+        for i in CMD_HELP:
+            string += "`" + str(i)
+            string += "`\t ❉  "
+        await event.edit(
+            "**⚡𝐒𝐊𝐘𝐙𝐔-𝐔𝐒𝐄𝐑𝐁𝐎𝐓​⚡**\n\n"
+            f"**◉ Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(modules)}**\n\n"
+            "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
+            f"◉ {string}◉\n\n✐ **ɴᴏᴛᴇꜱ :**  `{cmd}help animasi`\n✐  support : @skyzusupport"
+        )
+        await asyncio.sleep(1000)
+        await event.delete()

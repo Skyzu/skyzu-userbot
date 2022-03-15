02d075932c044d20bea96e1b52b783d98b143363
Skyzu <warsitowidodo4@gmail.com>
2022-03-15 15:36:45 +0000

    Skyzuu-Userbot: auto fixes
    Signed-off-by: Skyzu <warsitowidodo4@gmail.com>

--- userbot/modules/tags.py
+++ userbot/modules/tags.py
@@ -9,7 +9,8 @@ from telethon.tl.types import UserStatusOnline as onn
 from telethon.tl.types import UserStatusRecently as rec
 from telethon.utils import get_display_name
 
-from userbot import CMD_HELP, CMD_HANDLER as cmd
+from userbot import CMD_HANDLER as cmd
+from userbot import CMD_HELP
 from userbot.utils import skyzu_cmd
 
 

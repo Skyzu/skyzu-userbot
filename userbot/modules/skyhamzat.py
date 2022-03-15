66bbc613f64067b8091878d1716e6e3e11665c09
SkyzuProject <101615829+Skyzuxzy@users.noreply.github.com>
2022-03-15 22:38:52 +0700

    typo kontol

--- userbot/modules/skyhamzat.py
+++ userbot/modules/skyhamzat.py
@@ -75,7 +75,7 @@ async def _(hazmat):
 
 CMD_HELP.update(
     {
-        "hazmat": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`{cmd]hz` atau >`{cmd}hz [flip, x2, rotate (level), background (nomer), black]`"
+        "hazmat": f"𝘾𝙤𝙢𝙢𝙖𝙣𝙙: >`{cmd}hz` atau >`{cmd}hz [flip, x2, rotate (level), background (nomer), black]`"
         "\n↳ : Balas ke gambar/sticker untuk menyesuaikan."
     }
 )

848eaf7d4245533840780c7f5627a8d9a4293057
SkyzuProject <101615829+Skyzuxzy@users.noreply.github.com>
2022-03-23 20:23:20 +0700

    cange

--- userbot/modules/gcast.py
+++ userbot/modules/gcast.py
@@ -26,7 +26,7 @@ GCAST_BLACKLIST = [
     -1001481357570,  # UsergeOnTopic
     -1001459701099,  # CatUserbotSupport
     -1001109837870,  # TelegramBotIndonesia
-    -1001752592753,  # Skyzusupport
+    -1001602090466,  # Skyzusupport
     -1001380293847,  # NastySupport
     -1001267233272,  # PocongSupport
 ]

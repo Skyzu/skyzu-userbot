ec675b217660765e95633599f27ac9b3ece6f9f0
SkyzuProject <101615829+Skyzuxzy@users.noreply.github.com>
2022-05-02 20:01:55 +0700

    fixs chatbot

--- userbot/modules/chatbot.py
+++ userbot/modules/chatbot.py
@@ -14,7 +14,7 @@ from userbot.utils import skyzu_cmd
 translator = Translator()
 LANGUAGE = "id"
 
-url = "https://api-tede.herokuapp.com/api/chatbot?message={message}"
+url = "https://apitede.herokuapp.com/api/chatbot?message={message}"
 
 
 async def ngapain_rep(message):
